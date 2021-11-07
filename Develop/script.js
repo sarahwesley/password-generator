b9f403c82e49da4364ada29ff462e36da1a3f1ce
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-07 11:45:32 -0500

    readMe updated and variable arrays added

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+// variable arrays
+var number = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","#","$","%","&","'","(",")","*","+","'",",","/",":",";","<","=",">","?","@","]","[","_","|","}","~","`"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Password Generator Starter Code
+# Purpose
+## this website will allow the user to generate a random password by determining how long they want the password to be, if they want special characters, and if they want upper and/or lower case letters
+
+# Website:
+## add later
+
+# Code:
+## html, css, JavaScipt
+
+# Made by:
+## Sarah Wesley
