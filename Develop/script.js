df6a3ccfbe2ac2941eaa21296650e864ad376945
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-07 12:02:44 -0500

    character length function created, window prompt tells user length they chose.

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,29 @@
 var number = ["0","1","2","3","4","5","6","7","8","9"];
 var specialChar = ["!","#","$","%","&","'","(",")","*","+","'",",","/",":",";","<","=",">","?","@","]","[","_","|","}","~","`"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// declaration of variable
+
+var confirmLength = "";
+var confirmSpecialChar;
+var confirmNumericChar;
+var confirmUpperCase;
+var confirmLowerCase;
+
+// window prompt for user to choose password lenght
+function generatePassword() {
+  var confirmLength = (window.prompt("How many characters would your password to be?"));
+
+  while(confirmLength <= 7 || confirmLength >= 129) {
+    window.alert("Your password must be between 8-128 charactores. Please try again!");
+    var confirmLength = (window.prompt("How many characters would you like your password to be?"));
+  }
+
+  // tell the user how many characters their password will be
+  window.alert("Your password will have " + confirmLength + " characters.");
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
